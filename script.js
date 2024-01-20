466030cd53771178820c3df18a84148805345f65
raunak gupta <74643310+Raunakg2005@users.noreply.github.com>
2024-01-20 12:32:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add a click event listener to the "The Question" section
+document.getElementById("the-question").addEventListener("click", function() {
+  // Display a message asking the question
+  alert("Will you be my gf for this year too and another year too and after that too?");
+});
